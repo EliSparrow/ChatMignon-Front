7324ab6d177cf844fa524b155034c25944dac56b
EliSparow <max.pp@hotmail.fr>
2019-11-05 11:17:42 +0100

    Added Score Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './sass/index.sass'
 import './App.css';
+import { ListCats } from './component/catList';
 import { Vote } from './component/vote';
 import { Nav } from './component/nav';
 import { Footer } from './component/footer';
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <Route path="/" component={Nav}></Route>
+      <Route exact path="/score" component={ListCats}></Route>
       <Route exact path="/" component={Vote}></Route>
       <Route path="/" component={Footer}></Route>
     </Router>
